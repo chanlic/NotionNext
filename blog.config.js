8f6b2ca0cf3a27d4b092815b1c9568aad36def9a
cc l <cckeepgo@outlook.com>
2023-11-29 22:15:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '🪶Love rises with the wind, and the wind is difficult to settle🪶', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'cchili', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Sometime ever , sometimes never', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://cckeepgo.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, blog', // 网站关键词 英文逗号隔开
 
